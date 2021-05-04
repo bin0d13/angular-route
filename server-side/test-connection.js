b7dd937457ede8c30162bc1bd31395ad2d3728e8
binod <binodkumarsahu951@gmail.com>
2021-05-04 18:28:24 +0530

    initial commit

---
+++ server-side/test-connection.js
@@ -0,0 +1,6 @@
+//mongClient is a user define object name you can give any name for that.
+
+var mongoClient = require("mongodb").mongoClient;
+
+// mongdb connection string or url
+var url = "mongodb://127.0.0.1:27017";
